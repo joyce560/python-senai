b5c8df391d7042baf05957674cdc151af769597e
joyce560 <joycevitoria191@gmail.com>
2024-12-03 09:58:29 -0300

    aula 2

---
+++ AULAO2/AULA02.py
@@ -0,0 +1,11 @@
+print("bom dia galera")
+
+nome_usuario='joyce vitoria '# tipo -> str = strir 
+
+idade_usuario=21 # tipo C = interger 
+altura_usuario = 1.55 # tipo -> float= ponto flutuant 
+maior_idade= True # tipo -> bool= boolean 
+print ("seja bem vindo",nome_usuario) 
+print("sua idade é ,idade_usuario,'anos'
+print(sua altura é ,altura_usuario,"metros")
+print(voce é maior de idade ? ,maior_idade)
